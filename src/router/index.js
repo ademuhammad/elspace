73009e0cc247ecbaadfeced2638a3f1301d4dd9e
Adeveloper <ademnurhadi@gmail.com>
2025-07-08 23:17:52 +0700

    ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LandingPage from '../views/Landingpage.vue'
 import About from '@/views/About.vue'
 import Course from '@/views/Course.vue'
 import CourseDetail from '@/views/CourseDetail.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import RegisterPage from '@/views/RegisterPage.vue'
-
-// misalnya ada halaman register juga:
-// import RegisterPage from '../views/RegisterPage.vue'
 
 const routes = [
   { path: '/', name: 'Landing', component: LandingPage },
@@ -17,16 +14,14 @@ const routes = [
     path: "/Course/:id",
     name: "CourseDetail",
     component: CourseDetail,
-    props: true, // penting agar `id` dari URL jadi props
+    props: true,
   },
-
   { path: '/Register', name: 'Register', component: RegisterPage },
   { path: '/Login', name: 'Login', component: LoginPage }
-
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // ✅ GANTI DI SINI
   routes,
 })
 
